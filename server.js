a9bc46eb30a6d3aea226fceb498a7c6d474d7e4c
juli <julianmenav@gmail.com>
2023-01-26 16:05:14 +0100

    add hostname

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 
+const { hostname } = require('os');
 const port = 8080;
 
 app.use(express.static('public'))
@@ -9,6 +10,6 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/index.html');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname()}:${port}/`)
 })
